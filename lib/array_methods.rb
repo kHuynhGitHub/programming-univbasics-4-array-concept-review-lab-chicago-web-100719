de4edff8cfbf2f8a1e2e8edb0b852afa81292c47
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-09-26 18:06:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,6 +3,7 @@ def find_element_index(array, value_to_find)
     if array[count] == value_to_find
       return count
     end
+    nil
 end
 
 def find_max_value(array)
@@ -11,6 +12,7 @@ def find_max_value(array)
     if array[count] > max
       return max
     end
+    max
 end
 
 def find_min_value(array)
